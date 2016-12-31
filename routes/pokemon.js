067aa9378d7c332a087e8e061986b75676e14502
Tobias Murphy <murphy.tobias@gmail.com>
2016-12-30 16:25:12 -0800

    working except delete button

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
+var path = require('path');
+var request = require('request');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll({
+    order: [
+      ['id', 'ASC']
+    ]
+  }).then(function(pokemon){
+    res.render('pokemon', {pokemon: pokemon});
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+  db.pokemon.create(req.body).then(function(pokemon){
+    res.redirect("/pokemon");
+  });
+});
+
+
+//DELETE
+router.delete("/:id", function(req, res){
+  var deletePokemon = req.params.id;
+  db.pokemon.destroy({
+    where: { id: deletePokemon }
+  }).then(function(){
+    res.send("pokemon deleted");
+  });
 });
 
 module.exports = router;
